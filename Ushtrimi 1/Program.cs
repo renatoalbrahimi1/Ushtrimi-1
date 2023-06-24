d6eeb3bb5b94c44e324f659789eec4a6e58298d7
renatoalbrahimi1 <137329165+renatoalbrahimi1@users.noreply.github.com>
2023-06-24 17:04:09 +0200

    Add project files.

---
+++ Ushtrimi 1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Ushtrimi nr 1
+/*
+ using System;
+ internal class Program
+ {
+ public static void Main(string[] args)
+ {
+ Console.Write("emri:");
+ String emri = Console.ReadLine();
+ Console.Write("mbiemri:");
+ String mbiemri = Console.ReadLine();
+ Console.Write("mosha:");
+ int mosha = int.Parse(s: Console.ReadLine());
+ Console.Write("vendlindja:");
+ String vendlindja = Console.ReadLine();
+ Console.Write("profesioni:");
+ String profesioni = Console.ReadLine();
+
+ Console.WriteLine("Pershendetje, " + emri + " " + mbiemri + ".");
+
+ Console.WriteLine("Ju jeni " + mosha + " vjec.");
+
+ Console.WriteLine("Ju jeni nga " + vendlindja + ".");
+
+ Console.WriteLine("Profesioni që keni eshte " + profesioni + ".");
+    }
+ }
+*/
